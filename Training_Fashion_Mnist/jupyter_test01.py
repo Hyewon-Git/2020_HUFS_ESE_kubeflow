67c0af771f4dda03f4cecd3f2f75d5514990aa5c
Hye_won <49637333+hhhhyewon@users.noreply.github.com>
2020-06-12 05:00:31 +0900

    Update jupyter_test01.py

--- Training_Fashion_Mnist/jupyter_test01.py
+++ Training_Fashion_Mnist/jupyter_test01.py
@@ -23,6 +23,8 @@ class MyFashionMnist(object):
         fashion_mnist = keras.datasets.fashion_mnist
 
         (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+        train_images = train_images / 255.0
+        test_images = test_images / 255.0
 
         model = keras.Sequential([
             keras.layers.Flatten(input_shape=(28, 28)),
